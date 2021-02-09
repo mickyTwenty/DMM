f39bcf0a56b78c5b7fd9cd2f89efd784db5c6f5d
mickyTwenty <mickyTwenty@outlook.com>
2021-02-09 22:14:40 +0100

    Update AboutDialog.py

--- DMM/AboutDialog.py
+++ DMM/AboutDialog.py
@@ -12,7 +12,7 @@ class AboutDialog(QtWidgets.QDialog):
         self.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.FramelessWindowHint | QtCore.Qt.CustomizeWindowHint | QtCore.Qt.WindowTitleHint | QtCore.Qt.WA_ShowWithoutActivating)
         self.setModal(True)
 
-        QScroller.grabGesture(self.scrollArea, QScroller.LeftMouseButtonGesture)
+        #QScroller.grabGesture(self.scrollArea, QScroller.LeftMouseButtonGesture)
 
         self.ABOUT_TXT = 'about.txt'
 
